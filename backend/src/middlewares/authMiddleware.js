fd89614d1327c7c04411c193257b3aca87244ac9
Alvaro Ustarez <ale9622@gmail.com>
2022-01-25 17:37:56 -0300

    :tada: getUsers ok

--- backend/src/middlewares/authMiddleware.js
+++ backend/src/middlewares/authMiddleware.js
@@ -3,33 +3,41 @@ import asyncHandler from 'express-async-handler';
 import User from '../models/userModels.js';
 import config from '../config/index.js';
 
-export const protect =asyncHandler (async(req, res, next)=>{
+export const protect = asyncHandler(async(req, res, next) => {
     let token;
     if (
         req.headers.authorization &&
-        req.headers.authorization.startWith('Bearer')
-    ){
-        try{
+        req.headers.authorization.startsWith('Bearer')
+    ) {
+        try {
             token = req.headers.authorization.split(' ')[1];
-            const decoded = jwt.verify (token , config.jwtSecret);
-            req.user =await URLSearchParams.findById(decoded.id).select('-password');
+            const decoded = jwt.verify(token, config.jwtSecret);
+            req.user = await User.findById(decoded.id).select('-password');
             return next();
-        }catch (errror){
+        } catch (error) {
             console.error(error);
             res.status(401);
             throw new Error('Not authorized, token failed');
         }
     }
-    if (!token){
+    if (!token) {
         res.status(401);
-        throw new Error ('Not authorized, no token');
+        throw new Error('Not authorized, no token');
     }
 });
 
-export const admin = (req, res, next)=>{
+export const admin = (req, res, next) => {
     //Verificar si en el req viene el objeto user
     //con la propiedad isAdmin en true
     //y asignar el next()
     // sino retornar status 401
     // y arrojar el error:'Not authorized as an admin'
+
+    if (req.user.isAdmin) {
+        next();
+    } else {
+        res.status(401);
+        throw new Error('Not authorized as an admin');
+    }
+
 }
