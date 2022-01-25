b86cd47a2c284bf1c59029f52fe8e9b6d3d8dbc3
Marga <margaritazerpa18@gmail.com>
2022-01-25 12:40:10 -0300

    modificación controllers

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -53,3 +53,70 @@ export const registerUser = asyncHandler(async(req, res) => {
         throw new Error('Invalid user data')
     }
 });
+
+// @desc Get user profile
+// @route GET /api/users/profile
+// @access Private
+export const getUserProfile =asyncHandler(async (req, res)=>{
+    //Usar findById
+    //Evitar un res.json({}) que contenga: _id, name, email, isAdmin
+    //En caso de error devolver status 404 y arrojar el error: 'User not found'
+
+});
+
+// @decs Update user profile
+// @route PUT /api/users/profile
+// @access Private
+ export const updateUserProfile = asyncHandler(async (req, res)=>{
+     // Usar findById
+     // Asignar los valores que vienen de la req o del usuario encontrado
+     // ej: user.name =req.body.name || user.name
+     // si vienen el pasword en el req entonces asignarlo al user.password
+     // Guardar el usuario actualizado con .save()
+     // Enviar un res.json({}) que contenga: _id, name, email, is Admin, token
+     // En caso de error devolver status 404 y arrojar el error: 'User not found'
+ });
+
+ // @desc Get all users
+ // @route PUT /api/users
+ // @access Private/Admin
+ export const getUsers =asyncHandler(async (req, res) =>{
+     // Usar find
+     // Enviar un res.json() con el resultado
+ });
+
+ // @desc Delete user
+ // @route DELETE /api/users/:id
+ // @accsess Private/Admin
+ export const deleteUser = asyncHandler(async (req, res) =>{
+     // Usar findById
+     // Si se encontro el usuario usar .remove()
+     // Retornar un res.json({}) con el message: 'User removed'
+     // Si no se encontró el usuario retornar status 404
+     // Y arrojar el error: 'User not found'
+ });
+
+ // @desc Get user by ID
+ // @route GET /API/users/:id
+ // @access Private/Admin
+ export const getUserById= asyncHandler(async (req, res) =>{
+     // Usar findById agregandole .select() para evita el password
+     // Si existe el usuario regresar res.json() con el resultado
+     // Si o exixste el usuario retornar status 404
+     // Y rrojar el error: 'User not found'
+ });
+
+ // @decs Update user
+ // @route PUT /api/users/:id
+ // @access Private/Admin
+ export const updateUser = asyncHandler(async (req, res) =>{
+     // Usar findById
+     //Si se encontró el usuario entonces´:
+     //user.name = req.body.name || user.name;
+     //user.email = req.body.email || user.email;
+     //user.isAdmin = req.body.isAdmin || user.isAdmin;
+     //guardar con .save()
+     //Retornar un res.json({}) con contega: _id, name, email, isAdmin
+     //Si no se encontró el usuario entonces retornar status 404
+     //Y arrojar el error: 'User not found'
+ })
