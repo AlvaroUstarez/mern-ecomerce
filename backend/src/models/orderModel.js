e1b78dc7662fadcefac86448c376ad9c1276df16
Alvaro Ustarez <ale9622@gmail.com>
2022-01-26 15:33:02 -0300

    Update orderModel.js

--- backend/src/models/orderModel.js
+++ backend/src/models/orderModel.js
@@ -3,6 +3,9 @@ import mongoose from 'mongoose';
 const orderSchema = mongoose.Schema({
     user: {
         //Ref a user
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User',
     },
     orderItems: [{
         // name: Texto, requerido
@@ -10,21 +13,75 @@ const orderSchema = mongoose.Schema({
         // image: Texto, requerido
         // price: Numero, requerido
         // product: Ref a product
+        name: {
+            type: String,
+            required: true,
+        },
+        qty: {
+            type: Number,
+            required: true
+        },
+        image: {
+            type: String,
+            required: true
+        },
+        price: {
+            type: Number,
+            required: true
+        },
+        product: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: true,
+            ref: 'Product',
+        }
+
+
     }, ],
     shippingAdress: {
         // address: Texto, requerido
         // city: Texto, requerido
         // postalCode: Texto, requerido
         // country: Texto, requerido
+        address: {
+            type: String,
+            required: true
+        },
+        city: {
+            type: String,
+            required: true
+        },
+        postalCode: {
+            type: String,
+            required: true
+        },
+        country: {
+            type: String,
+            required: true
+        }
     },
     paymentMethod: {
         // Texto, requerido
+        type: String,
+        required: true
     },
     paymentResult: {
         // id:Texto
         // status: Texto
         // update_time: Texto
         // email_address: Texto
+        id: {
+            type: String,
+        },
+        status: {
+            type: String,
+        },
+        update_time: {
+            type: String,
+        },
+        email_address: {
+            type: String,
+        }
+
     },
     taxPrice: {
         type: Number,
