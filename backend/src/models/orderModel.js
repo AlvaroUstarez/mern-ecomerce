c6cb5a5330e683a6268b3fa29f438f9801ccda55
Alvaro Ustarez <ale9622@gmail.com>
2022-01-26 15:13:27 -0300

    creando orderModel
    creando el esquema de orderModel

--- backend/src/models/orderModel.js
+++ backend/src/models/orderModel.js
@@ -1,60 +1,57 @@
 import mongoose from 'mongoose';
 
-const orderSchema= mongoose.Schema(
-    {
-        user: {
-            //Ref a user
-        },
-        orderItems:[
-            {
-                // name: Texto, requerido
-                // qty: Numero, requerido
-                // image: Texto, requerido
-                // price: Numero, requerido
-                // product: Ref a product
-            },
-        ],
-        shippingAdress:{
-            // address: Texto, requerido
-            // city: Texto, requerido
-            // postalCode: Texto, requerido
-            // country: Texto, requerido
-        },
-        paymentMethod:{
-            // Texto, requerido
-        },
-        paymentResult:{
-            // id:Texto
-            // status: Texto
-            // update_time: Texto
-            // email_address: Texto
-        },
-        taxPrice:{
-            // Numero, requerido
-            // Default 0.0
-        },
-        totalPrice:{
-            // Numero, requerido
-            // Default 0.0
-        },
-        isPaid:{
-            // Boolean, requerido
-            // Default false
-        },
-        paidAt:{
-            // Fecha
-        },
-        isDelivered:{
-            // Boolean, requerido
-            // Default false
-        },
-        DeliveredAt:{
-            // Fecha
-        },
+const orderSchema = mongoose.Schema({
+    user: {
+        //Ref a user
     },
-    {
-        timestamp:true,
-    }
-);
+    orderItems: [{
+        // name: Texto, requerido
+        // qty: Numero, requerido
+        // image: Texto, requerido
+        // price: Numero, requerido
+        // product: Ref a product
+    }, ],
+    shippingAdress: {
+        // address: Texto, requerido
+        // city: Texto, requerido
+        // postalCode: Texto, requerido
+        // country: Texto, requerido
+    },
+    paymentMethod: {
+        // Texto, requerido
+    },
+    paymentResult: {
+        // id:Texto
+        // status: Texto
+        // update_time: Texto
+        // email_address: Texto
+    },
+    taxPrice: {
+        type: Number,
+        default: 0.0,
+    },
+    totalPrice: {
+        type: Number,
+        default: 0.0
+    },
+    isPaid: {
+        type: Boolean,
+        required: true,
+        default: false,
+    },
+    paidAt: {
+        type: Date
+    },
+    isDelivered: {
+        type: Boolean,
+        required: true,
+        default: false,
+    },
+    DeliveredAt: {
+        type: Date
+    },
+}, {
+    timestamp: true,
+});
 const Order = mongoose.model('Order', orderSchema);
 export default Order;
