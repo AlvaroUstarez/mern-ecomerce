2afa511f5931de928aa9524f2fecd713f5d03658
Alvaro Ustarez <ale9622@gmail.com>
2022-01-26 13:54:13 -0300

    Update productController.js
    desafios productController creados

--- backend/src/controllers/productController.js
+++ backend/src/controllers/productController.js
@@ -7,14 +7,12 @@ import Product from '../models/productModel';
 export const getProducts = asyncHandler(async(req, res) => {
     const pageSize = 10; // test 2
     const page = Number(req.query.pageNumber) || 1;
-    const keyword = req.query.keyword ?
-        {
-            name: {
-                $regex: req.query.keyword,
-                $options: 'i',
-            },
-        } :
-        {};
+    const keyword = req.query.keyword ? {
+        name: {
+            $regex: req.query.keyword,
+            $options: 'i',
+        },
+    } : {};
     const count = await Product.countDocuments({...keyword });
     const products = await Product.find({...keyword })
         .limit(pageSize)
@@ -53,6 +51,69 @@ export const createProduct = asyncHandler(async(req, res) => {
         price: 0,
         user: req.user._id,
         image: '/images/sample.jpg',
+        brand: 'Sample brand',
+        category: 'Sample category',
+        countInStock: 0,
+        numReviews: 0,
+        description: 'Sample desciption',
+    });
+    const createdProduct = await product.save();
+    res.status(201).json(createdProduct);
+});
 
-    })
-})
+//@desc Update a product
+//@route PUT /api/products/:id
+//@access Private/Admin
+export const updateProduct = asyncHandler(async(req, res) => {
+    //Obtener name, price, description, image, brand, category y countInStock
+    //del req.body
+    //Usar findById
+    //Si existe el producto asignar los datos de la sigiente forma:
+    //product.name name ?? product.name;
+    //Usar .save()
+    //Retornar res.json() con el producto actualizado
+    //Si no existe el producto retornar status 404
+    //Y arrojar el error: 'Product Not Found'
+});
+
+//@desc Create new review
+//@route POST /api/products/:id/reviews
+//@access Private
+export const createProductReview = asyncHandler(async(req, res) => {
+    const { rating, comment } = req.body;
+    const product = await Product.findById(req.params.id);
+    if (product) {
+        const alreadyReviewed = product.reviews.find(
+            (r) => r.user.toString() === req.user._id.toString()
+        );
+        if (alreadyReviewed) {
+            res.status(400);
+            throw new Error('Product already reviewed');
+        }
+        const review = {
+            name: req.user.name,
+            rating: Number(rating),
+            comment,
+            user: req.user._id,
+        };
+        product.reviews.push(review);
+        product.numReviews = product.reviews.length;
+        product.rating =
+            product.reviews.reduce((acc, item) => item.rating + acc, 0) /
+            product.reviews.length;
+        await product.save();
+        res.status(201).json({ message: 'Review added' });
+    } else {
+        res.status(404);
+        throw new Error('Product Not Foutd');
+    }
+});
+
+//@desc Get top rated products
+//@route GET /api/products/top
+//@access Public
+export const getTopProducts = asyncHandler(async(req, res) => {
+    //Usar find ({}) y sort para ordenar por los más rankeados
+    //y un limit de 3 resultados solamente
+    //Retornar un json con los productos encontrados
+});
