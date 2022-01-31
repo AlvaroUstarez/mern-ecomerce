d52931debf3268d6b9b5cd7d66a80c00c7b16255
Nancy <nancy.toconas@gmail.com>
2022-01-31 17:08:15 -0300

    correcion bug

--- frontend/src/routes/AppRouter.js
+++ frontend/src/routes/AppRouter.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Container } from 'react-bootstrap/lib/Tab';
+//import { Container } from 'react-bootstrap/lib/Tab';
+import { Container } from 'react-bootstrap';
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Header from '../components/Header';
 import HomePage from '../pages/HomePage';
